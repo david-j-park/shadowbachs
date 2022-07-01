dc1aed20cb91b262b5e8c8c15285ac5f600b9a78
David Park <david@howdidothat.info>
2022-07-01 09:32:23 -0400

    Derp

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         punch on keys. Rich vocal harmonies round out the sound that begs you to move.</p>
         </div>
         <div className="col-1_3" style={{paddingTop: '25px', paddingBottom: '25px'}}>
-        <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/album/6nt4zUiasBqR5A3Pk2uxGH?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/album/6nt4zUiasBqR5A3Pk2uxGH?utm_source=generator" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         <p><a href="https://music.apple.com/us/album/shadow-bachs/1632324591"><img src="/ListenOnApple.svg"></img></a></p>
         </div>
       </div>
